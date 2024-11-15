dfaf0f26e70418641b776866b955700111750ba0
fauzan2912 <e0960156@u.nus.edu>
2024-11-15 18:59:52 +0800

    Updated files

--- README.md
+++ README.md
@@ -1,8 +1,52 @@
 # 7-teens-DSA3101-2410-Project
-Repository for 7-teens DSA3101 2410 Project
+# Repository for 7-teens DSA3101 2410 Project
+
+# Project Overview
+E-commerce platforms like Shopee Singapore face numerous challenges in optimizing operations, understanding customer behavior, and maximizing profitability. This project aims to harness advanced data analysis techniques to gain deep insights into various facets of the e-commerce business. By focusing on sales trends, customer segmentation, inventory management, and pricing strategies, we will develop a robust analytical framework. This framework will empower e-commerce businesses to make informed, data-driven decisions that enhance performance, boost customer satisfaction, and drive revenue growth.
+
+# Project Objective
+The primary objective of this project is to create a comprehensive data analysis framework that enables Shopee Singapore to optimize its operations and strategic decision-making processes. Our team of eight data scientists seek to address key business questions, including:
+
+Customer Behavior and Sales Analysis: To identify key factors influencing customer purchasing behavior, improve customer retention and lifetime value, and determine the most effective marketing channels and campaigns. This involves analyzing historical sales data, developing customer segmentation models, calculating churn rates, and evaluating the ROI of different marketing strategies.
+
+Inventory Management and Pricing Optimization: To optimize inventory levels to minimize costs while ensuring product availability, implement effective pricing strategies to maximize revenue, and enhance supply chain efficiency. This includes developing demand forecasting models, creating inventory optimization algorithms, analyzing price elasticity, and optimizing order fulfillment processes.
+
+By addressing these business questions, we aim to provide actionable insights that will drive Shopee Singapore’s growth and operational excellence. Our comprehensive analysis framework will enable the company to make data-driven decisions that enhance customer satisfaction, streamline operations, and increase profitability.
 
 
-Docker Instructions:
+# Instructions for running the Docker container
+Follow these steps to set up and run the project using Docker.
 
-docker build -t 7-teens .
-docker run -p 8888:8888 -v $(pwd):/app 7-teens
+## Prerequisites
+- Ensure you have [Docker](https://docs.docker.com/get-docker/) installed.
+
+## Step-by-Step Guide
+
+### Step 1: Clone the Repository
+Clone the repository to your local machine and navigate to the project directory:
+```
+git clone https://github.com/7-teens/7-teens-DSA3101-2410-Project/
+cd 7-teens-DSA3101-2410-Project
+```
+### Step 2: Build the Docker Image
+Build the Docker image by running the following command in the project directory:
+```
+docker build -t <image-name> .
+```
+Replace ```<image-name>``` with a descriptive name for the Docker image (e.g., ```project-image```).
+
+### Step 3: Run the Docker Container
+Run the Docker container with this command:
+```
+docker run -p 8888:8888 -v $(pwd):/app <image-name>
+```
+Explanation:
+```-p 8888:8888``` maps port ```8888``` on your local machine to port ```8888``` inside the container.
+```-v $(pwd):/app``` mounts the current directory (```$(pwd)```) to ```/app``` inside the container, allowing you to see any file changes in real-time.
+
+### Step 4: Access the Application
+Once the container is running, open a web browser and go to:
+```
+http://localhost:8888
+```
+
--- Subgroup B/Prophet_Forecast.py
+++ Subgroup B/Prophet_Forecast.py
@@ -3,7 +3,7 @@ import plotly.graph_objects as go
 import numpy as np
 from datetime import timedelta, datetime
 
-def forecast_next_n_months(train_df, regressors, n, current_stock, current_produce_cost, current_holding_cost, lead_time, product_price):
+def forecast_next_n_months(train_df, lead_time, regressors=None, n=1):
     df = train_df.groupby('order_time')['daily_sales'].sum().reset_index()
     df.columns = ['ds', 'y']
     for event, dates in regressors.items():
@@ -58,115 +58,117 @@ def forecast_next_n_months(train_df, regressors, n, current_stock, current_produ
         )
 
         fig.show()
-    
-    def plot_inventory_strategy(forecast, current_stock, current_produce_cost, current_holding_cost, lead_time):
-        forecast['inventory_level'] = current_stock
-        optimal_inventory_levels = [current_stock]
-        optimal_reorder_points = []
-        optimal_reorder_labels = []
-        optimal_order_costs = []
-        optimal_delayed_orders = {}
-        optimal_reorder_lock = False
-
-        for i, row in forecast.iterrows():
-            current_optimal_inventory = optimal_inventory_levels[-1]
-            reorder_cost_optimal = 0
-
-            if not optimal_reorder_lock:
-                projected_inventory = current_optimal_inventory
-                future_demand = forecast['prophet_safety_stock'][i:i + lead_time].sum()
-                if (projected_inventory - future_demand) <= row['prophet_safety_stock']:
-                    reorder_amount_optimal = int(future_demand - projected_inventory)
-                    reorder_cost_optimal = reorder_amount_optimal * current_produce_cost
-                    optimal_reorder_points.append((row['ds'], current_optimal_inventory))
-                    optimal_reorder_labels.append(f"+{reorder_amount_optimal}")
-                    arrival_day = i + lead_time
-                    optimal_delayed_orders[arrival_day] = optimal_delayed_orders.get(arrival_day, 0) + reorder_amount_optimal
-                    optimal_reorder_lock = True
-
-            optimal_order_costs.append(reorder_cost_optimal + current_optimal_inventory * current_holding_cost)
-            current_optimal_inventory = max(current_optimal_inventory - int(row['yhat']), 0) + optimal_delayed_orders.get(i, 0)
-
-            if i in optimal_delayed_orders:
-                optimal_reorder_lock = False
-            optimal_inventory_levels.append(current_optimal_inventory)
-
-        forecast['optimal_inventory_level'] = optimal_inventory_levels[:-1]
-        forecast['optimal_order_cost'] = np.cumsum(optimal_order_costs)
-
-        # Calculate final cumulative profit for each strategy
-        optimal_order_final_cost = forecast['optimal_order_cost'].iloc[-1]
-        actual_final_revenue = (forecast['yhat'] * product_price).sum()
-
-        # Calculate profits
-        optimal_order_profit = actual_final_revenue - optimal_order_final_cost
-
-        fig = go.Figure()
-
-        fig.add_trace(go.Scatter(
-            x=forecast['ds'], y=forecast['optimal_inventory_level'],
-            mode='lines', name="Optimal Inventory Level", line=dict(color='orange', dash='dash')
-        ))
-
-        fig.add_trace(go.Scatter(
-            x=[point[0] for point in optimal_reorder_points],
-            y=[point[1] for point in optimal_reorder_points],
-            mode='markers+text', marker_symbol='star', marker_size=10, marker_color='orange',
-            text=optimal_reorder_labels, textposition="top center",
-            name="Optimal Reorder Points"
-        ))
-
-        fig.add_trace(go.Scatter(
-            x=forecast['ds'], y=forecast['prophet_safety_stock'],
-            mode='lines', name="Safety Stock Level", line=dict(color='red', dash='dot')
-        ))
-
-        fig.update_layout(
-            title=f"Optimal Inventory Levels, Reorder Points, and Safety Stock Levels for {start_date_title} to {end_date_title}",
-            xaxis_title="Date",
-            yaxis_title="Inventory / Demand",
-            legend=dict(x=1.02, y=1, bordercolor="Black", borderwidth=1),
-            template="plotly_white",
-            showlegend=True
-        )
-
-        fig.show()
-
-        fig_cost = go.Figure()
-
-        fig_cost.add_annotation(
-        x=forecast['ds'].iloc[-1], y=optimal_order_final_cost,
-        text=f"Optimal Profit: ${optimal_order_profit:,.2f}", showarrow=True, arrowhead=1, font=dict(color="orange")
-        )
-
-        # Optimal Order Cumulative Cost
-        fig_cost.add_trace(go.Scatter(
-            x=forecast['ds'], y=forecast['optimal_order_cost'],
-            mode='lines', name="Optimal Strategy Cumulative Cost", line=dict(color='orange', dash='dash')
-        ))
-
-        # Actual Cumulative Revenue
-        fig_cost.add_trace(go.Scatter(
-            x=forecast['ds'], y=np.cumsum(forecast['yhat'] * product_price),
-            mode='lines', name="Forecasted Cumulative Revenue", line=dict(color='green')
-        ))
-
-        fig_cost.update_layout(
-            title=f"Forecasted Cumulative Cost and Revenue for {start_date_title} to {end_date_title}",
-            xaxis_title="Date",
-            yaxis_title="Cumulative Cost and Revenue",
-            legend=dict(x=1.02, y=1, bordercolor="Black", borderwidth=1),
-            template="plotly_white",
-            showlegend=True
-        )
-
-        fig_cost.show()
 
     plot_forecast(forecast)
-    plot_inventory_strategy(forecast, current_stock, current_produce_cost, current_holding_cost, lead_time)
-
     return forecast
 
+def plot_inventory_strategy(forecast, current_stock, current_produce_cost, current_holding_cost, lead_time, product_price):
+    forecast['inventory_level'] = current_stock
+    optimal_inventory_levels = [current_stock]
+    optimal_reorder_points = []
+    optimal_reorder_labels = []
+    optimal_order_costs = []
+    optimal_delayed_orders = {}
+    optimal_reorder_lock = False
+
+    start_date = forecast['ds'].min()
+    end_date = forecast['ds'].max()
+    start_date_title = start_date.strftime("%d %B %Y")
+    end_date_title = end_date.strftime("%d %B %Y")
+
+    for i, row in forecast.iterrows():
+        current_optimal_inventory = optimal_inventory_levels[-1]
+        reorder_cost_optimal = 0
+
+        if not optimal_reorder_lock:
+            projected_inventory = current_optimal_inventory
+            future_demand = forecast['prophet_safety_stock'][i:i + lead_time].sum()
+            if (projected_inventory - future_demand) <= row['prophet_safety_stock']:
+                reorder_amount_optimal = int(future_demand - projected_inventory)
+                reorder_cost_optimal = reorder_amount_optimal * current_produce_cost
+                optimal_reorder_points.append((row['ds'], current_optimal_inventory))
+                optimal_reorder_labels.append(f"+{reorder_amount_optimal}")
+                arrival_day = i + lead_time
+                optimal_delayed_orders[arrival_day] = optimal_delayed_orders.get(arrival_day, 0) + reorder_amount_optimal
+                optimal_reorder_lock = True
+
+        optimal_order_costs.append(reorder_cost_optimal + current_optimal_inventory * current_holding_cost)
+        current_optimal_inventory = max(current_optimal_inventory - int(row['yhat']), 0) + optimal_delayed_orders.get(i, 0)
+
+        if i in optimal_delayed_orders:
+            optimal_reorder_lock = False
+        optimal_inventory_levels.append(current_optimal_inventory)
+
+    forecast['optimal_inventory_level'] = optimal_inventory_levels[:-1]
+    forecast['optimal_order_cost'] = np.cumsum(optimal_order_costs)
+
+    # Calculate final cumulative profit for each strategy
+    optimal_order_final_cost = forecast['optimal_order_cost'].iloc[-1]
+    actual_final_revenue = (forecast['yhat'] * product_price).sum()
+
+    # Calculate profits
+    optimal_order_profit = actual_final_revenue - optimal_order_final_cost
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=forecast['ds'], y=forecast['optimal_inventory_level'],
+        mode='lines', name="Optimal Inventory Level", line=dict(color='orange', dash='dash')
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=[point[0] for point in optimal_reorder_points],
+        y=[point[1] for point in optimal_reorder_points],
+        mode='markers+text', marker_symbol='star', marker_size=10, marker_color='orange',
+        text=optimal_reorder_labels, textposition="top center",
+        name="Optimal Reorder Points"
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=forecast['ds'], y=forecast['prophet_safety_stock'],
+        mode='lines', name="Safety Stock Level", line=dict(color='red', dash='dot')
+    ))
+
+    fig.update_layout(
+        title=f"Optimal Inventory Levels, Reorder Points, and Safety Stock Levels for {start_date_title} to {end_date_title}",
+        xaxis_title="Date",
+        yaxis_title="Inventory / Demand",
+        legend=dict(x=1.02, y=1, bordercolor="Black", borderwidth=1),
+        template="plotly_white",
+        showlegend=True
+    )
+
+    fig.show()
+
+    fig_cost = go.Figure()
+
+    fig_cost.add_annotation(
+    x=forecast['ds'].iloc[-1], y=optimal_order_final_cost,
+    text=f"Optimal Profit: ${optimal_order_profit:,.2f}", showarrow=True, arrowhead=1, font=dict(color="orange")
+    )
+
+    # Optimal Order Cumulative Cost
+    fig_cost.add_trace(go.Scatter(
+        x=forecast['ds'], y=forecast['optimal_order_cost'],
+        mode='lines', name="Optimal Strategy Cumulative Cost", line=dict(color='orange', dash='dash')
+    ))
+
+    # Actual Cumulative Revenue
+    fig_cost.add_trace(go.Scatter(
+        x=forecast['ds'], y=np.cumsum(forecast['yhat'] * product_price),
+        mode='lines', name="Forecasted Cumulative Revenue", line=dict(color='green')
+    ))
+
+    fig_cost.update_layout(
+        title=f"Forecasted Cumulative Cost and Revenue for {start_date_title} to {end_date_title}",
+        xaxis_title="Date",
+        yaxis_title="Cumulative Cost and Revenue",
+        legend=dict(x=1.02, y=1, bordercolor="Black", borderwidth=1),
+        template="plotly_white",
+        showlegend=True
+    )
+
+    fig_cost.show()
 
     
 
